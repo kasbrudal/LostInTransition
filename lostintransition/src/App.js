0b47bdda3ecb4f290ffcd728976f1520ab450c3e
kasbrudal <peephyt@gmail.com>
2023-08-15 14:26:54 +0200

    filestructur

--- lostintransition/src/App.js
+++ lostintransition/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card'
+
+import loginForm from './Components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Card>
+        <loginForm />
+      </Card>
     </div>
+    
+    
   );
 }
 
---
+++ lostintransition/src/Components/Login/Login.jsx
@@ -0,0 +1,4 @@
+function loginForm() {
+   return <input type="text">Skriv in tekst</input>
+}
+export default loginForm
