f5eeae7a7132cb5a47792388f71e3e85a21df795
Jacob Langevei <jacob-langevei@hotmail.com>
2023-08-17 14:30:27 +0200

    Router in Header

--- lostintransition/src/Components/Header.jsx
+++ lostintransition/src/Components/Header.jsx
@@ -1,7 +1,19 @@
-function navBar(){
-    
-    return <h1>Lost in translation</h1>
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
+import { LoginPage } from '../Pages/LoginPage';
+import { TranslatePage } from '../Pages/TranslatePage';
 
-
-}
+export function navBar(){  
+	return (
+		<div>
+			<BrowserRouter>
+				<NavLink to="/">Lost in translation</NavLink> <br/>
+				<NavLink to="/translate">Translate</NavLink>
+		
+				<Routes>
+					<Route path={'/'} element={<LoginPage/>}></Route>
+					<Route path={'/Translate'} element={<TranslatePage/>}></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+)}
 export default navBar
