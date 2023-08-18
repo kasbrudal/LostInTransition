dc30d037a4635af1bf192ff29b899fd8b0902fe1
kasbrudal <peephyt@gmail.com>
2023-08-18 08:28:01 +0200

    login

--- lostintransition/src/Components/Login.jsx
+++ lostintransition/src/Components/Login.jsx
@@ -1,38 +1,60 @@
-import { Form, FormControl, FormGroup, FormLabel } from "react-bootstrap"
-export function loginForm() {
-   const apiURL = 'https://lost-in-translation-production-9e97.up.railway.app'
-   const apiKey = 'experis'
+import React, { useState } from "react";
+import { Form, FormControl, FormGroup, FormLabel, Button } from "react-bootstrap";
 
-   fetch(`${apiURL}/translations`, {
-         method: 'POST',
-            headers: {
-            'X-API-Key': apiKey,
-            'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ 
-                username: 'mega-mind', 
-                translations: [] 
-            })
+function LoginForm() {
+  const [username, setUsername] = useState('');
+
+  const handleInputChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleUpload = () => {
+    if (username.trim() !== '') {
+      const apiURL = 'https://lost-in-translation-production-9e97.up.railway.app';
+      const apiKey = 'experis';
+
+      fetch(`${apiURL}/translations`, {
+        method: 'POST',
+        headers: {
+          'X-API-Key': apiKey,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 
+          username: username, 
+          translations: [] 
         })
-        .then(response => {
+      })
+      .then(response => {
         if (!response.ok) {
-            throw new Error('Could not create new user')
+          throw new Error('Could not create new user');
         }
-        return response.json()
-        })
-        .then(newUser => {
-        // newUser is the new user with an id
-        })
-        .catch(error => {
-        })
-   return ( 
-      <Form>
-         <FormGroup className="mb-3">
-            <FormLabel>Write your name</FormLabel>
-            <FormControl type="name" placeholder="Write here"/>
-         </FormGroup>
-      </Form>
-   )
-   
+        return response.json();
+      })
+      .then(newUser => {
+        // Handle successful response here
+        console.log('New user:', newUser);
+      })
+      .catch(error => {
+        // Handle error here
+        console.error('Error:', error.message);
+      });
+    }
+  };
+
+  return ( 
+    <Form>
+      <FormGroup className="mb-3">
+        <FormLabel>Write your name</FormLabel>
+        <FormControl 
+          type="text" 
+          placeholder="Write here" 
+          value={username}
+          onChange={handleInputChange}
+        />
+        <Button onClick={handleUpload}>Upload</Button>
+      </FormGroup>
+    </Form>
+  );
 }
-export default loginForm
+
+export default LoginForm;
