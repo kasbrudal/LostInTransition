fdd102ce9f671a07049369f147bd49ea64ada95c
Jacob Langevei <jacob-langevei@hotmail.com>
2023-08-18 13:49:55 +0200

    TranslationsList profilePage
    logic for displayed 10 last translations on profile page

--- lostintransition/src/Components/TranslationsList.jsx
+++ lostintransition/src/Components/TranslationsList.jsx
@@ -1,7 +1,43 @@
-export function TranslationsList() {
+import { useState, useEffect } from 'react';
+import Translation from './Translation.jsx';
+
+function TranslationsList() {
+    const [translations, setTranslations] = useState([]);
+    const apiURL = 'https://lost-in-translation-production-9e97.up.railway.app';
+    const apiKey = 'experis';
+    const currentUser = 'mega-mind';
+    
+    useEffect (() => {
+        fetch(`${apiURL}/translations?username=${currentUser}`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            setTranslations(data.slice(-10));
+        })
+        .catch(error => console.error(error.message))
+    }, []);
+
+    const handleClearTranslations = () => {
+        setTranslations([]);
+    };
+
     return (
         <div>
-            <li>heihei</li>
+            <ul>
+                {translations && translations.map((translation, index) => (
+                    <li key={index}>{translation.text}
+                        <img 
+                            src={require(`../assets/individial_signs/${translation.imgValue}.png`)}  
+                            alt={translation.imgValue}
+                        />
+                    </li>
+                ))}
+            </ul>
+            {/*{translations && translations.map(translation => 
+            <Translation apiKey={translation.id} currentTranslation={translation}></Translation>)}*/}
         </div>
     )
 }
